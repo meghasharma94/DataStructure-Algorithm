86f77bdcb13a4131603af7b592fc66c158066c51
Megha  Sharma <megha.sharmaa94@gmail.com>
2022-05-28 15:36:47 +0200

    Array rotation and arrangement questions

---
+++ src/main/java/Arrays/arrangements/RearrangeArray.java
@@ -0,0 +1,54 @@
+package Arrays.arrangements;
+
+import java.util.Arrays;
+
+/**
+ * Rearrange array in alternating positive & negative items with O(1) extra space
+ * <p>
+ * Input:  arr[] = {1, 2, 3, -4, -1, 4}
+ * Output: arr[] = {-4, 1, -1, 2, 3, 4}
+ * <p>
+ * Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+ * output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0}
+ */
+public class RearrangeArray {
+
+    public static void main(String[] args) {
+        //int arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8};
+        int arr[]={1, 2, 3, -4, -1, 4};
+       Arrays.toString(rearrange(arr));
+
+    }
+
+
+    private static void print(int[] arr) {
+        System.out.println(" ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void rotate(int[] arr, int start, int end) {
+        int temp = arr[end];
+        for (int i = end; i > start; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[start] = temp;
+    }
+
+    private static int[] rearrange(int[] arr) {
+
+        int p = 0, q = p + 1;
+
+        while (p <= arr.length && q <= (arr.length -1)) {
+
+            if (arr[q] < 0) {
+                rotate(arr, p, q);
+                print(arr);
+                p = p + 2;
+            }
+            q++;
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/Arrays/rotation/FindIndex.java
@@ -0,0 +1,54 @@
+package Arrays.rotation;
+
+/**
+ *
+ * Input : arr[] : {1, 2, 3, 4, 5}
+ *         ranges[] = { {0, 2}, {0, 3} }
+ *         index : 1
+ * Output : 3
+ * Algorithm
+ *
+ * So,  3 cases :
+ * If index = left, index will be changed to right.
+ * If index is not bounds by the range, no effect of rotation.
+ * If index is in bounds, index will have the element at index-1.
+ *
+ * */
+public class FindIndex {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // No. of rotations
+        int rotations = 2;
+
+        // Ranges according to 0-based indexing
+        int[][] ranges = {{0, 2}, {0, 3}};
+
+        int index = 1;
+        System.out.println(findElement(arr, ranges,
+                rotations, index));
+    }
+
+    private static int findElement(int[] arr, int[][] ranges,
+                                   int rotations, int index) {
+        for (int i = rotations - 1; i >= 0; i--) {
+
+            // Range[left...right]
+            int left = ranges[i][0];
+            int right = ranges[i][1];
+
+            // Rotation will not have any effect
+            if (left <= index && right >= index) {
+                if (index == left)
+                    index = right;
+                else
+                    index--;
+            }
+        }
+
+        // Returning new element
+        return arr[index];
+    }
+
+}
